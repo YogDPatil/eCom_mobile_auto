b7acbe37ccd227bcee93912b26cc8a3b5ee3ab0c
YogDPatil <ydpatil.it@gmail.com>
2025-05-14 21:11:28 +0530

    meesho app intial setup code

---
+++ src/test/java/com/ui/tests/FirstTest.java
@@ -0,0 +1,16 @@
+package com.ui.tests;
+
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.Activity;
+
+public final class FirstTest extends TestBase {
+
+	@Test
+	public void validateTest() {
+		
+		
+		
+		}
+
+}
---
+++ src/test/java/com/ui/tests/TestBase.java
@@ -0,0 +1,61 @@
+package com.ui.tests;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+public abstract class TestBase {
+	private DesiredCapabilities caps;
+	protected AndroidDriver androidDriver;
+	private AppiumDriverLocalService service;
+
+	@BeforeMethod(alwaysRun = true)
+	public void intialiseAppiumServer() {
+		Map<String, String> env = new HashMap(System.getenv());
+		caps = new DesiredCapabilities();
+		caps.setCapability("platformName", "Android");
+		caps.setCapability("automationName", "UiAutomator2");
+		caps.setCapability("app", System.getProperty("user.dir") + "/src/test/resources/testing_app/meesho.apk");
+		service = new AppiumServiceBuilder()
+				.withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"))
+				.withIPAddress("127.0.0.1").usingPort(4723).withEnvironment(env).build();
+		service.start();
+		try {
+			androidDriver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), caps);
+		} catch (MalformedURLException | URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+		// Start the main activity explicitly (not usually needed if app already opens
+		// to this)
+//	    Map<String, Object> args = new HashMap<>();
+//	    args.put("appPackage", "com.meesho.supply");
+//	    args.put("appActivity", "com.meesho.supply.main.HomeActivity");
+//	    androidDriver.executeScript("mobile: startActivity", args);
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		androidDriver.quit();
+		service.stop();
+	}
+
+}
